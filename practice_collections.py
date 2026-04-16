ad2e5519234cba973ce946d124e5b01042ada6f3
tyzwhitt <tyzwhitt@gmail.com>
2026-04-15 21:49:21 -0300

    # %% working with py files and interactive window

---
+++ practice_collections.py
@@ -0,0 +1,41 @@
+# Nearest stars to Earth
+# star1 = 'Sol'
+# star2 = 'Alpha Centauri'
+# star3 = 'Barnard'
+# star4 = 'Wolf 359'
+
+# # Highest peak on each tectonic plate
+# African = 'Kilimanjaro'
+# Antarctic = 'Vinson'
+# Australian = 'Puncak Jaya'
+# Eurasian = 'Everest'
+# North_American = 'Denali'
+# Pacific = 'Mauna Kea'
+# South_American = 'Aconcagua'
+
+# %%
+stars = ['Sol',
+         'Alpha Centauri',
+         'Barnard',
+         'Wolf 359']
+
+print(stars[3])
+
+peaks = {'African': 'Kilimanjaro',
+         'Antarctic': 'Vinson',
+         'Australian': 'Puncak Jaya',
+         'Eurasian': 'Everest',
+         'North_American': 'Denali',
+         'Pacific': 'Mauna Kea',
+         'South_American': 'Aconcagua'}
+
+print(peaks['Pacific'])
+
+print(peaks['Australian'])
+
+i = 0
+for x in stars:
+    i += 1
+    print(stars)
+print(i)
+# %%
